ec54cfc2c28a10823efd71a3990b22760dce45d2
Klaus Pedersen <klaus.pedersen@onlineconcept.dk>
2022-10-28 10:45:01 +0200

    list of tags

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const core = require('@actions/core');
-const {GitHub, context} = require('@actions/github');
+const {context} = require('@actions/github');
+const {Octokit} = require("@octokit/rest");
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const github = new GitHub(process.env.GITHUB_TOKEN);
   const serviceName = core.getInput('service-name');
   const initialVersion = core.getInput('initial-version');
   console.log(`The service name is ${serviceName}!`);
   console.log(`The initial version is ${initialVersion}!`);
-  
   //Here comes the code to update the version
   const newTag = initialVersion;
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN,
+  });
 
-  
+//  const { data: { test } } = octokit.repos.getReleaseByTag();
+  const tags = octokit.repos.listTags({
+    owner: context.payload.repository.owner.login,
+    repo: context.payload.repository.name,
+  });
+  console.log(JSON.stringify(tags))
 
   
   //outputs
